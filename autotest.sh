f2357f3a9e9a377541bccb54ed1f84b088dafccc
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2019-07-04 03:43:58 -0700

    Added function to set calling directory on autotest script

--- autotest.sh
+++ autotest.sh
@@ -2,11 +2,25 @@
 
 function main()
 {
+  set_calling_dir_global
   while true
   do
     find lib test -type f |
       entr -d bash entr_script.sh
   done
+}
+
+function set_calling_dir_global()
+{
+  local current_folder
+  current_folder=$(pwd)
+
+  create_global_readonly "CALLING_DIR" "$current_folder"
+}
+
+function create_global_readonly()
+{
+  declare -gr "$1=$2"
 }
 
 main "$@"
